f6905b9b67b3ffd5263a6932ebf5feaacd24f186
aadilMohamed <72676882+aadilMohamed@users.noreply.github.com>
2020-11-02 08:56:14 +0800

    Add files via upload

---
+++ p5.play-boilerplate-master (2)/p5.play-boilerplate-master/bullets and walls.js
@@ -0,0 +1,37 @@
+var bullet
+var wall
+
+function setup() {
+  createCanvas(1600,400);
+bullet = createSprite(100,200,40,30)
+wall = createSprite(1500,200,50,300)
+speed = bullet.velocityX
+bullet.velocityX = random(15,40)
+wall.width = random(22,83)
+bullet.shapeColor = color("black")
+}
+
+function draw() {
+  background(255,255,255);
+if(hasCollided(bullet,wall)){
+  bullet.velocityX = 0
+  var damage = 0.5*(bullet.velocityX*bullet.velocityX/(wall.width*wall.width*wall.width));
+  if(damage>10){
+    wall.shapeColor=color("green");
+  }
+  if(damage<10){
+    wall.shapeColor=color("red");
+  }
+}
+bullet.depth = wall.depth+1
+  drawSprites();
+}
+
+function hasCollided(lbullet,lwall){
+bulletRightEdge = lbullet.x+ lbullet.width;
+wallLeftEdge = lwall.x
+if(bulletRightEdge>=wallLeftEdge){
+  return true
+}
+return false
+}
